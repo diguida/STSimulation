6f979d77a56e54b55e9cd89cf702d8b6ce54ad92
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2016-07-18 20:35:39 +0200

    Added simple test for LCG class.

---
+++ test_LinearCongruentialGenerator.cpp
@@ -0,0 +1,15 @@
+#include "include/LinearCongruentialGenerator.h"
+#include <iostream>
+
+int main( int argc, char** argv ) {
+  LinearCongruentialGenerator lcg( 57 );
+  for( unsigned int i = 0; i < 10; ++i ) {
+    double r = lcg.random();
+    std::cout << "Random number: " << r << std::endl;
+  }
+  for( unsigned int i = 0; i < 10; ++i ) {
+    double exp_r = lcg.exp( 127 );
+    std::cout << "Random exp: " << exp_r << std::endl;
+  }
+  return 0;
+}
