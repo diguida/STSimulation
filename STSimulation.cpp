89edd797a501183170340bbacebcbc4e38ad22a4
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2016-08-03 15:57:12 +0200

    Print results of the pre-run phase.

--- STSimulation.cpp
+++ STSimulation.cpp
@@ -25,25 +25,57 @@ int main(  int argc, char** argv ) {
   unsigned long long seed = 57;
   std::shared_ptr<MS> p_stub = std::make_shared<MS>( seed );
   TD testDriver{ p_stub };
+  std::cout << "************************ GENERATOR ************************\n"
+            << "* Modulus: " << Pars::m << "\n"
+            << "* Multiplier: " << Pars::a << "\n"
+            << "* Increment: " << Pars::c << "\n"
+            << "* Input seed: " << seed << "\n"
+            << "***********************************************************"
+            << std::endl;
 
   // Part 2: The pre-run phase:
-  // * the number of initial defects is initial_faults;
-  // * the number of failure to be observed is observations_pre_run; this is the number of random interfailure times to be produced iteratively via the test driver.
-  // * the lifetime has an exponential distribution with fixed rate lambda_pre_run;
+  // * the number of initial defects is initialFaults;
+  // * the lifetime has an exponential distribution with fixed rate lambda_preRun;
+  // * the number of failure to be observed is observations_preRun; this is the number of random interfailure times to be produced iteratively via the test driver.
   unsigned int initialFaults = 60;
+  double lambda_preRun = 0.00785; //failures per hour
   std::size_t observations_preRun = 100;
   std::vector<double> interfailureTimes_preRun;
   interfailureTimes_preRun.reserve( observations_preRun );
-  double lambda_preRun = 0.00785; //failures per hour
   testDriver.setRate( lambda_preRun );
+  std::cout << "********************** PRE-RUN PHASE **********************\n"
+            << "* Number of initial faults: " << initialFaults << "\n"
+            << "* Initial (constant) rate of failures: " << lambda_preRun << " failures/h\n"
+            << "* Number of failures to be observed: " << observations_preRun <<"\n"
+            << "***********************************************************"
+            << std::endl;
   double time_preRun = 0.;
   for( std::size_t i = 0; i < observations_preRun; ++i ) {
-    std::cout << "Testing the MID: looking for failure " << i << std::endl;
+    std::cout << "Testing the MID: running up to failure " << i + 1 << std::endl;
     double t = testDriver.callMID();
     interfailureTimes_preRun.push_back( t );
     time_preRun += t;
-    std::cout << "Inter-failure time in the pre-run phase: " << t << std::endl;
+    std::cout << "Inter-failure time between failure " << i
+              << " and " << i + 1
+              << " in the pre-run phase: " << t << std::endl;
+    std::cout << "Time spent in executing the MID till failure " << i + 1
+              << ": " << time_preRun << std::endl;
   }
   std::cout << "Total time spent in the pre-run phase: " << time_preRun << " hours." << std::endl;
+  // Part 3: Extracting the rate of failure per faults:
+  // ratio between the number of observed failure in the pre-run phase
+  // and the product of the initial faults and the time spent in the pre-run
+  // phi_0 = observations_preRun / ( initialFaults * time_preRun )
+  double phi_0 = (double) observations_preRun / ( initialFaults * time_preRun );
+  std::cout << "********************* PRE-RUN RESULTS *********************\n"
+            << "* Total time spent in the pre-run phase: " << time_preRun << " h\n"
+            << "* Inizial rate of failure per faults: " << phi_0 << " failures/(h*fault)\n"
+            << "***********************************************************"
+            << std::endl;
+  
+  // Part 4: The run phase
+  // * the number of initial defects is initialFaults;
+  // * the number of failure to be observed is observations; this is the number of random interfailure times to be produced iteratively via the test driver;
+  // * the rate of failure per faults is phi_0, obtained in the pre-run phase // 
   return 0;
 }
